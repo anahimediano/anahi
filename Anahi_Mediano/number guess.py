75e1568de656e32dd398b8250fbec92275c6e471
anahimediano <anahimediano28@gmail.com>
2024-09-12 15:43:46 -0600

    strings file

--- Anahi_Mediano/number guess.py
+++ Anahi_Mediano/number guess.py
@@ -1,18 +1,14 @@
 import random
 number= random.randint(1,10)
-print("guess a number between 1 and 10.")
-     while True:
-             number_guess=input("enter your guess (type ´quit´to exit):")
-            if inputt.lower()=="quit":
-                print("The number was" + number + "Thanks for playing!")
-                break 
-            try:
-            number_guess = int(inputt)
-            if number_guess=number
-                print("Congratulations! You've guessed the right number!")
-                break
-            else:
-                print("Wrong guess, try again.")
-
-        except ValueError:
-            print("Invalid input. Please enter a number or type 'quit' to exit.")
+print ("guess a number between 1 and 10:")
+while True:
+    number_guess=input("enter your guess(type quit to exit):")
+    print ("The number was" + [number])
+    break
+if number_guess == number
+    print ("Congratualations! you´ve guessed the right number!")
+    break
+else:
+    print("wrong guess, try again.")
+except ValueError:
+    print("Invalid input. Please enter a number or type quit to exit")
