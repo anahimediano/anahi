461e8e745dc1add65f082231c51ee3c31734180b
anahimediano <anahimediano28@gmail.com>
2024-09-19 15:35:14 -0600

    RPS file

--- Anahi_Mediano/rock_paper_scissors.py
+++ Anahi_Mediano/rock_paper_scissors.py
@@ -1,19 +1,25 @@
 import random
-options = ("rock","paper","scissors")
+def result(text):
+        print(f"**********{text}**********")
+options = ["1","2","3","rock","paper","scissors"]
+def RPS():
+        while True:
+                user_ch=input("Choose rock(1), paper(2) or scissors(3):")
+                computer_ch=random.choice(options)
+                print(computer_ch)
+    
+                if user_ch not in options:
+                        result("not allowed, please enter a valid option")
+                        continue
+                
+                if user_ch=="quit":
+                        result("bye")
+                        break
+                if user_ch==computer_ch:
+                        result("is a tie, try again")
+                elif((user_ch=="rock" or "1") and computer_ch=="scissors") or ((user_ch== "paper" or "2") and computer_ch=="rock") or ((user_ch=="scissors" or "3") and computer_ch=="paper"):
+                        result("Congratulations, you won!")
+                else:
+                        result("you lose")
+RPS()
 
-while True:
-    user_ch=input("Choose rock, paper or scissors")
-    coputer_ch=random.choice(options)
-    print ("choose rock, paper or scissors")
-    if user_ch=="quit":
-        break
-    if user_ch=="":
-            print ("choose rock, paper, scissors")
-            continue
-    if user_ch==coputer_ch:
-            print("tie")
-    elif(user_ch=="rock" and coputer_ch=="scissors") or (user_ch== "paper" and coputer_ch=="rock") or (user_ch=="scissors" and coputer_ch=="paper"):
-            print("Congratulations, you won!")
-    else:
-            print("you lose")
-            
